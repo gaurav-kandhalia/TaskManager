14e1b47394c6168a057e8ccdc998019e6e7c6af3
Gaurav Kandhalia <vaidkandhala333@gmail.com>
2025-02-13 19:53:06 +0530

    bug fixed

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -145,15 +145,10 @@ const logoutUser = asyncHandler(async(req, res) => {
         $unset: { refreshToken: 1 }
     });
 
-    const options = {
-        httpOnly: true,
-        secure: true
-    };
+
 
     return res
-        .status(200)
-        .clearCookie("accessToken", options)  
-        .clearCookie("refreshToken", options)  
+        .status(200)  
         .json(new ApiResponse(200, {}, "User logged out successfully"));
 });
 
